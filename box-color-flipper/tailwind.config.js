9ee767fb5e96430fe1284d04b8941ba5db3a7da4
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2021-11-01 09:41:24 +0530

    Add files via upload

--- box-color-flipper/tailwind.config.js
+++ box-color-flipper/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ["./public/index.html"],
+    content: ["./dist/index.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
